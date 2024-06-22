0fbe753bcd26dd9512b147d20c477f362fb70968
Madan <madan.codewing@mgial.com>
2024-06-22 21:16:19 +0545

    added css

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,31 +3,40 @@
 
 <head>
     <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+
+
 </head>
 
 <body>
-    <h1>Login</h1>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="login-page">
+
+        <div class="wrapper">
+            <h1>Login</h1>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password">
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+
     </div>
-    @endif
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div>
-            <label>Email</label>
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
-        <div>
-            <label>Password</label>
-            <input type="password" name="password">
-        </div>
-        <button type="submit">Login</button>
-    </form>
 </body>
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with Vite</title>
+    <title>Bundeena Store</title>
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/scss/style.scss'])
 </head>
