d369adfcb81d2da6da85d8e1fd89e97635cda46d
Madan <madan.codewing@mgial.com>
2024-07-01 23:58:42 +0545

    code refinements

--- resources/js/pages/Home/index.jsx
+++ resources/js/pages/Home/index.jsx
@@ -1,86 +1,9 @@
 import React from 'react';
 import { Card, Carousel, SectionHeader, CTA } from '../../components';
 import { bannerImage, cta } from '../../components/images'
+import { sliderContent, offerCard, categoryList, serviceList } from '../../components/data'
 
 const Home = () => {
-    const sliderContent = [
-        {
-            title: "Fueling Your Future",
-            description: "Discover the power of high-quality fuels that keep your engines running longer and cleaner. Join the thousands of satisfied customers who trust us for their fuel needs.",
-            buttonText: "Learn More",
-            link: "#",
-            image: bannerImage,
-        },
-        {
-            title: "Eco-Friendly Fuels",
-            description: "Commit to a greener planet with our eco-friendly fuel options. Reduce emissions and protect the environment without compromising performance.",
-            buttonText: "Explore More",
-            link: "#",
-            image: bannerImage,
-        },
-    ];
-
-    const offerCard = [
-        {
-            title: "Eco-Friendly Fuels",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            date: "Offer valid from: 30/05/2024 - 03/07/2024",
-            image: bannerImage,
-        },
-        {
-            title: "Eco-Friendly Fuels",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            date: "Offer valid from: 30/05/2024 - 03/07/2024",
-            image: bannerImage,
-        },
-        {
-            title: "Eco-Friendly Fuels",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            date: "Offer valid from: 30/05/2024 - 03/07/2024",
-            image: bannerImage,
-        },
-    ]
-
-    const categoryList = [
-        {
-            link: "#",
-            image: bannerImage,
-            name: "Fuel"
-        },
-        {
-            link: "#",
-            image: bannerImage,
-            name: "Coffee"
-        },
-        {
-            link: "#",
-            image: bannerImage,
-            name: "Food"
-        },
-        {
-            link: "#",
-            image: bannerImage,
-            name: "Service"
-        }
-    ]
-
-    const serviceList = [
-        {
-            title: "Car Wash",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            image: cta,
-        },
-        {
-            title: "Car Wash",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            image: cta,
-        },
-        {
-            title: "Car Wash",
-            description: "Cadbury Medium Bar 30g - 60g varieties",
-            image: cta,
-        },
-    ]
 
     return (
         < >
