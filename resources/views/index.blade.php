459ed5150dedd7320df6f310b92c8bc03b29ae07
Madan <madan.codewing@mgial.com>
2024-07-10 23:41:20 +0545

    fix favicon issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bundeena Store</title>
-    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/images/favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{asset('resources/images/favicon.ico') }}">
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/scss/style.scss'])
 </head>
